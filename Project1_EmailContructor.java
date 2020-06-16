6ab826381f597184e8856786a2efb5e0910a755b
GaryMcCullyUWGB <mccuga01@uwgb.edu>
2020-06-16 17:24:16 +0000

    Fixed typos in the code that prevented the initial code from executing.

--- Project1_EmailContructor.java
+++ Project1_EmailContructor.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-public class EMailContructor
+public class Project1_EmailContructor
 {
-  public static void main(Strings args)
+  public static void main(String[] args)
 
   {
-    String[] paramters=new String[4]; //Java starts array indexing at 0.
+    String[] parameters=new String[4]; //Java starts array indexing at 0.
     Scanner user_input= new Scanner(System.in);
 
     System.out.println("This program takes a series of user supplied paramters\n and outputs the SMTP Commands Used to Send an e-mail\n");
