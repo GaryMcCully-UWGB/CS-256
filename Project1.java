d6eb2959bacb82707138df5a08add7d8ef4a3ad0
GaryMcCully-UWGB <66743028+GaryMcCully-UWGB@users.noreply.github.com>
2020-06-16 12:08:12 -0500

    Create Project1.java
    This is the initial file that contains the outline of the code I will be building upon using repl.it.

---
+++ Project1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class EMailContructor
+{
+  public static void main(Strings args)
+
+  {
+    String[] paramters=new String[4]; //Java starts array indexing at 0.
+    Scanner user_input= new Scanner(System.in);
+
+    System.out.println("This program takes a series of user supplied paramters\n and outputs the SMTP Commands Used to Send an e-mail\n");
+    System.out.println("Please enter SMTP Server");
+    
+    parameters[0]=user_input.nextLine();
+    
+    System.out.println(parameters[0]);
+  }
+}
